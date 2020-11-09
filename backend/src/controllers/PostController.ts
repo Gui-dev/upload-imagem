f8c1a494dd9dbb5976679a105ef3906d43697aea
Gui-dev <adrianon10@gmail.com>
2020-11-09 19:53:33 -0300

    feat: create PostController and helpers methods index, create and delete

--- backend/src/controllers/PostController.ts
+++ backend/src/controllers/PostController.ts
@@ -1,17 +1,37 @@
 import { Request, Response } from "express"
-import { Post } from './../models/Post'
+import Post from './../models/Post'
+
+export interface MulterFile {
+  originalname: string
+  size: number
+  key: string // Available using `S3`.
+  location: string // Available using `DiskStorage`.
+}
 
 export class PostController {
 
   public async index(request: Request, response: Response) {
-
+    const posts = await Post.find()
+    return response.status(201).json(posts)
   }
 
   public async create(request: Request, response: Response) {
 
+    const { originalname: name, size, filename } = request.file
+    const post = await Post.create({
+      name,
+      size,
+      image: filename,
+    })
+
+    return response.status(201).json(post)
   }
 
   public async delete(request: Request, response: Response) {
+    const { id } = request.params
+    const post = await Post.findById(id)
 
+    await post?.remove()
+    return response.status(200).send()
   }
 }
